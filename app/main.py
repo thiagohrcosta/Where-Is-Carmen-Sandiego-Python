53aa899f63ec64934f048e9a57cceb0527141810
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2023-05-01 16:16:03 -0300

    Fix some bugs on game flow

--- app/main.py
+++ app/main.py
@@ -71,11 +71,11 @@ def main():
     detective_experience = 0
     detective_life = 5
 
-    setup()
     while detective_life > 0 and carmen_sandiego_life > 0:
         setup()
         first_message()
-        
+
+    setup()
     if detective_life < 0:
       print("Carmen Sandiego scape and nobody knows where she is... you lose!")
       print("GAME OVER")
@@ -89,25 +89,19 @@ def first_message():
     global detective_life
     global carmen_sandiego_life
 
-    print(
-        "======================================================================="
-    )
+    print("=======================================================================")
     if detective_experience < 3:
         print("You position at the agency is: Rookie")
     elif detective_experience >= 3 and detective_experience < 6:
         print("You position at the agency is: Detective")
     elif detective_experience >= 6 and detective_experience <= 10:
         print("You position at the agency is: Senior Detective")
-    print(
-        "======================================================================="
-    )
+    print("=======================================================================")
 
     print("Detective " + detective_name + ", you have " +
           str(detective_experience) + " experience points.")
 
-    print(
-        "======================================================================="
-    )
+    print("=======================================================================")
 
     print("")
     print("")
@@ -115,11 +109,9 @@ def first_message():
         "************************* URGENT MESSAGE ******************************"
     )
     print("")
-    print("Carmen Sandiego has stolen a valuable object from the museum!")
+    print("Carmen Sandiego has stolen a valuable object!")
     print("We need you to find her and recover the object.")
-    print(
-        "======================================================================="
-    )
+    print("=======================================================================")
     print("Your phone is ringing...")
     print(f"Hi detective " + detective_name + ", I'm the chief of the agency.")
     print("We receive a message:")
@@ -127,6 +119,8 @@ def first_message():
 
 
 def carmen_sandiego_action():
+    global detective_life
+
     carmen_location = random.choice(places.list_of_places)
     carmen_are_at = random.choice(countries.list_of_countries)
 
@@ -147,9 +141,7 @@ def carmen_sandiego_action():
     )
     print(f"a country where the flag is {carmen_are_at['flag_color']}.")
     print("")
-    print(
-        "======================================================================="
-    )
+    print("=======================================================================")
     print("")
     print(
         "You have to find her and recover the object. What country do you think she is? \n"
@@ -172,11 +164,11 @@ def carmen_sandiego_action():
         )
         print("You are wrong! She scape from you.")
         print("You loose 1 life point.")
-        detective_life = -1
+        detective_life -= 1
         print(f"You have {detective_life} life points.")
-        print(
-            "======================================================================="
-        )
+        print("=======================================================================")
+        if detective_life < 1:
+            print("You have no life points. GAME OVER")
 
 
 def trying_to_arrest_carmen_sandiego():
@@ -184,23 +176,15 @@ def trying_to_arrest_carmen_sandiego():
     global carmen_sandiego_life
 
     if detective_experience < 3:
-        print(
-            f"You have only {detective_experience}, she is too strong for you. She scape from you."
-        )
+        print(f"You have only {detective_experience}, she is too strong for you. She scape from you.")
         print("You don't loose life points this time.")
         carmen_sandiego_life = carmen_sandiego_life - 1
 
-        print(
-            f"Carmen Sandiego loose 1 life point. Now she has {carmen_sandiego_life} life points."
-        )
+        print(f"Carmen Sandiego loose 1 life point. Now she has {carmen_sandiego_life} life points.")
         print("You gain 3 experience points.")
         detective_experience += 3
-        print(
-            "======================================================================="
-        )
-        print(
-            f"LEVEL UP! You are now a Detective! You have {detective_life} life points and {detective_experience} experiences."
-        )
+        print("=======================================================================")
+        print(f"LEVEL UP! You are now a Detective! You have {detective_life} life points.")
     else:
         print("You have enough experience to arrest her.")
         print("You gain 5 experience points.")
@@ -208,15 +192,10 @@ def trying_to_arrest_carmen_sandiego():
         carmen_sandiego_life -= 3
         detective_experience += 5
 
-        print(
-            f"Carmen Sandiego loose 3 life points. Now she has {carmen_sandiego_life} life points."
-        )
+        print(f"Carmen Sandiego loose 3 life points. Now she has {carmen_sandiego_life} life points.")
 
 
 def setup():
-    print(
-        f"Detective life: {detective_life}                              Carmen Sandiego life: {carmen_sandiego_life}"
-    )
-
+    print(f"Detective life: {detective_life}                              Carmen Sandiego life: {carmen_sandiego_life}")
 
 main()
