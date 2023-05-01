8caea73138ba4e8c07e7a810aae66a75d3e55a06
ThiagoHRCosta <thiagohrcosta86@gmail.com>
2023-05-01 16:05:26 -0300

    First version with country selection by flag and detective and carmen points of life

---
+++ app/places.py
@@ -0,0 +1,52 @@
+list_of_places = [
+    {
+        "id": 1,
+        "name": "Museun",
+        "action": "stolen a valuable object from the museum!"
+    },
+    {
+        "id": 2,
+        "name": "Airport",
+        "action": "bought a ticket to"
+    },
+    {
+        "id": 3,
+        "name": "Train Station",
+        "action": "bought a ticket to"
+    },
+    {
+        "id": 4,
+        "name": "Hotel",
+        "action": "checked in at"
+    },
+    {
+        "id": 5,
+        "name": "Restaurant",
+        "action": "ate at"
+    },
+    {
+        "id": 6,
+        "name": "Bank",
+        "action": "made a deposit at"
+    },
+    {
+        "id": 7,
+        "name": "Hospital",
+        "action": "was seen at"
+    },
+    {
+        "id": 8,
+        "name": "Park",
+        "action": "trying to hide at"
+    },
+    {
+        "id": 9,
+        "name": "House",
+        "action": "trying to hide at"
+    },
+    {
+        "id": 10,
+        "name": "Boat",
+        "action": "trying to scape on"
+    }
+]
